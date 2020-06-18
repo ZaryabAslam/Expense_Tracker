407f7f8c8e39c6c8aaa20b5a596f12bcdc70b802
ZaryabAslamJatt <66242220+ZaryabAslamJatt@users.noreply.github.com>
2020-06-19 00:17:08 +0500

    tracker app

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <>
+      <b><h1 className="Headerkiheading">Expense Tracker</h1></b>
+      <h1 className="made-by">By: Zaryab Aslam</h1>
+      <hr />
+    </>
+  );
+};
